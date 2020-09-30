e8f11524104caa879fc798dc14dc98dee940abae
mpvaitheeswaran <mpvaitheeswaran>
2020-09-30 21:50:24 +0530

    First Commit

---
+++ index.php
@@ -0,0 +1,282 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link href="https://fonts.googleapis.com/css2?family=Meera+Inimai&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="icon" href="img/spkc-logo.jpeg">
+    <title>SPKC</title>
+  </head>
+  <body>
+    <img src="img/background.jpg" alt="">
+
+  <div class="header">
+    <div class="logo">
+      <img src="img/spkc-logo.jpeg" alt="">
+      
+      <p>ஸ்ரீ பரமகல்யாணி கல்லூரி</p>
+
+    </div>
+    <div class="header-desc">
+      <p>Cent Percent Character & Cent Percent Efficiency</p>
+    </div>
+  </div>
+    <!--Nav bar-->
+
+      <ul id="menu">
+        <li><a class="active" href="#home">Home</a></li>
+        <li><a href="#">Administration</a>
+          <ul>
+            <li><a href="#">Governing Body</a></li>
+            <li><a href="#">College committee</a></li>
+            <li><a href="#">College Council</a></li>
+            <li><a href="#">Secretary</a></li>
+            <li><a href="#">Principal</a></li>
+          </ul>
+        </li>
+        <li><a href="#">Admission</a>
+          <ul>
+            <li><a href="#">Aided</a>
+              <ul>
+                <li><a href="#">Courses Offered</a></li>
+                <li><a href="#">Department Profile</a>
+                  <ul>
+                    <li><a href="#">Mathematics</a></li>
+                    <li><a href="#">Physics</a></li>
+                    <li><a href="#">Chemistry</a></li>
+                    <li><a href="#">Zoology</a></li>
+                    <li><a href="#">Commerce</a></li>
+                    <li><a href="#">Microbiology</a></li>
+                    <li><a href="#">English</a></li>
+                    <li><a href="#">Tamil</a></li>
+                  </ul>
+                </li>
+                <li><a href="#">Office</a></li>
+                <li><a href="#">Apply Now</a></li>
+                <li><a href="#">Prospectus</a></li>
+              </ul>
+            </li>
+            <li><a href="#">Self Financing</a>
+              <ul>
+                <li><a href="#">Courses Offered</a></li>
+                <li><a href="#">Department Profile</a>
+                  <ul>
+                    <li><a href="#">Maths</a></li>
+                    <li><a href="#">Computer Science</a></li>
+                    <li><a href="#">Computer Application</a></li>
+                    <li><a href="#">Biotech</a></li>
+                    <li><a href="#">Commerce</a></li>
+                    <li><a href="#">English</a></li>
+                    <li><a href="#">Electronics & Comunication</a></li>
+                  </ul>
+                </li>
+                <li><a href="#">Office</a></li>
+                <li><a href="#">Apply Now</a></li>
+                <li><a href="#">Prospectus</a></li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+        <li><a href="#">Facilities</a>
+          <ul>
+            <li><a href="#">Library</a></li>
+            <li><a href="#">Computer Lab</a></li>
+            <li><a href="#">Sports</a></li>
+            <li><a href="#">Hostel Boys & Girls</a></li>
+            <li><a href="#">Core-operative store</a></li>
+
+          </ul>
+        </li>
+        <li><a href="#">IQAC & SSR</a>
+          <ul>
+            <li><a href="#">NAAC SSR Cycle 2</a></li>
+            <li><a href="#">AQAR Report</a></li>
+            <li><a href="#">Member List</a></li>
+            <li><a href="#">IQAC Minutes</a></li>
+            <li><a href="#">Calender</a></li>
+
+          </ul>
+        </li>
+        <li><a href="#">Extension Service</a>
+          <ul>
+            <li><a href="#">NCC</a></li>
+            <li><a href="#">NSS</a></li>
+            <li><a href="#">RRC</a></li>
+            <li><a href="#">Literary Club</a></li>
+            <li><a href="#">Eco Club</a></li>
+            <li><a href="#">Career Guidance Cell</a></li>
+            <li><a href="#">YRC</a></li>
+            <li><a href="#">Woman Redressal</a></li>
+            <li><a href="#">Annti Ragging</a></li>
+            <li><a href="#">Students Council</a></li>
+            <li><a href="#">Councelling Scheme</a></li>
+          
+          </ul>
+        </li>
+        <li><a href="#">About Us</a>
+          <ul>
+            <li><a href="#">Vision & Mission</a></li>
+            <li><a href="#history">Histroy</a></li>
+            <li><a href="#contact">Contact Us</a></li>
+            <li><a href="#">Gallery</a></li>
+            <li><a href="#">Events</a></li>
+          </ul>
+        </li>
+      </ul>
+
+    <!--Nav bar-->
+    <div class="wellcome-img" id="home">
+      
+    </div>
+
+
+    <div class="wellcome-area">
+      <div class="text-part">
+        <p>
+          Sri Paramakalyani College is affiliated to Manonmaniam Sundaranar University, Tirunelveli. It offers a wide range of academic programmes and provides a learning environment for students and faculty that fosters intellectual, social and personal development.
+
+          The SPKC mission statement that describes our Institution as the one that is fully committed to excellence, innovation and education anchored in the field of education.
+        </p>
+      </div>
+
+    </div>
+
+
+    <div class="course-detail">
+      <div class="course-section aided">
+        <h1>Aided Courses</h1>
+        <div class="courses">
+          <div class="ug-courses">
+            <h3>UG Courses</h3>
+            <ul>
+              <li><a href="#">B.Sc Mathematics</a></li>
+              <li><a href="#">B.Sc Physics</a></li>
+              <li><a href="#">B.Sc Chemistry</a></li>
+              <li><a href="#">B.Sc Zoology</a></li>
+              <li><a href="#">B.Com</a></li>
+            </ul>
+          </div>
+          <div class="pg-courses">
+            <h3>PG Courses</h3>
+            <ul>
+              <li><a href="#">M.Sc Physics</a></li>
+                <li><a href="#">M.Sc Chemistry</a></li>
+                <li><a href="#">M.Sc Microbiology</a></li>
+              </ul>
+          </div>
+        </div>
+      </div>
+      <div class="course-section self-finance">
+        <h1>Self Finance Courses</h1>
+        <div class="courses">
+          <div class="ug-courses">
+            <h3>UG Courses</h3>
+            <ul>
+              <li><a href="#">B.Sc Mathematics</a></li>
+              <li><a href="#">B.Sc Computer & Science</a></li>
+              <li><a href="#">BCA</a></li>
+              <li><a href="#">B.Sc Bio - Technology</a></li>
+              <li><a href="#">B.Sc Electronics & Communications</a></li>
+              <li><a href="#">B.A English</a></li>
+              <li><a href="#">B.Com</a></li>
+              <li><a href="#">B.Com Corporate Secretaryship</a></li>
+              
+            </ul>
+          </div>
+          <div class="pg-courses">
+            <h3>PG Courses</h3>
+            <ul>
+              <li><a href="#">M.Com</a></li>
+              <li><a href="#">M.Sc Mathematics</a></li>
+              <li><a href="#">M.Sc Computer Science</a></li>
+            </ul>
+          </div>
+              
+            
+        </div>
+      </div>
+      <div class="course-section announce">
+        <h1>Announcement</h1><br>
+          <ul>
+            <li><a href="#">Rank List 2020-2021</a></li>
+            <li><a href="#">List of PG Aplicant</a></li>
+            <li><a href="#">PG Selection List</a></li>
+            <li><a href="#">Pay fees for II and III year</a></li>
+          </ul>
+      </div>
+    </div>
+
+      <div class="history-area" id="history">
+        
+        <div class="text-part">
+          <h1>History</h1>
+          <p>
+          
+            Sri Paramakalyani College was founded in 1963 by Late Sri S. Anantha ramakrishnan, a doyen of Industry in South India and a great visionary. The college is run by the Sri Paramakalyani Education Society, with Sri A. Sivasailam (Padmashree awardee) as President and Sri A. Krishnamoorthy as a member of the governing body.
+            <br><br>
+            The Vedic adage “Through Knowledge to Immortality” is the mission of our institution. With cent percent efficiency and cent percent character as our goal, we strive to provide value based, quality higher education at an affordable cost to the people living in and around Alwarkurichi. The steady growth of the institution is attributed mainly with the healthy practices and values that are being followed in administering the institution.
+            <br><br>
+            Apart from the college, the Education Society runs a Higher Secondary School with 1800 students in its roll, 15 bedded hospital providing quality medicare free of cost, a Kalyanamandapam and also a Training Institute for Diamond Cutting.
+          </p>
+      
+        </div>
+     </div>
+
+    <div class="footer">
+      <div class="footer-content">
+        <div class="footer-section about">
+          <h2>Address</h2>
+          <p>
+            Sri Paramakalyani Collage,<br>
+            Sivasailam Road,<br>
+            Alwarkurichi – 627 412. <br>
+            Tenkasi District. <br>
+            Tamilnadu.
+          </p>
+        </div>
+        <div class="footer-section links">
+          <h2>Quick Links</h2>
+          <ul>
+            <li><a href="#">Alumni</a></li>
+            <li><a href="#">Exam Result</a></li>
+            <li><a href="#">Admission</a></li>
+            <li><a href="#">Administration</a></li>
+            <li><a href="#">Gallery</a></li>
+          </ul>
+        </div>
+        <div class="footer-section contact" id="contact">
+          <h2>Contact</h2>
+          <h4>Aided Office</h4>
+          <span><i class="material-icons" style="vertical-align: -6px;">phone</i> &nbsp 04634 290626 </span>
+          <span><i class="material-icons" style="vertical-align: -6px;">email</i> &nbsp  spkcollegeoffice@gmail.com </span>
+          <h4>Self Finance Office</h4>
+          <span><i class="material-icons" style="vertical-align: -6px;">phone</i> &nbsp 04634 290017 </span>
+          <span><i class="material-icons" style="vertical-align: -6px;">email</i> &nbsp spkcsf@gmail.com </span>
+        </div>
+       
+      </div>
+      <div class="footer-bottom">
+        Copyright &copy; 2020 SPKC | Designed By Vaitheeswaran
+      </div>
+    </div>
+
+    <script>
+      window.onscroll = function() {myFunction()};
+
+      var navbar = document.getElementById("menu");
+      var sticky = navbar.offsetTop;
+
+      function myFunction() {
+        if (window.pageYOffset >= sticky) {
+          navbar.classList.add("sticky")
+        } else {
+          navbar.classList.remove("sticky");
+        }
+      }
+    </script>
+
+  </body>
+</html>
